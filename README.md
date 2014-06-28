653516a51ff74f9a8f010bc3bfaa8903594deb68
PanGa <pangalz@gmail.com>
2014-06-28 11:18:05 -0300

    Updating vert.x to 2.1.1 and to use Nashorn from JDK 8u5

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 Node.js vs vert.x
 =================
 
-**WORK IN PROGRESS**
-
 Simple and naive Node.js vs vert.x benchmark. I've written it to check if there's any significant performance difference between Node.js (which is getting more popular than even before and undoubtedly more stable and mature) and new kid on the block -- vert.x (which shares the asynchronous and message-oriented philosophy with Node.js). I'm playing with Node since v0.2, this benchmark took me 2 hours of work mostly due to trial-and-error approach to vert.x, so the code may not be the most optimized and sophisticated.
 
 Node.js server uses Restify framework and Mongoose ORM. Vert.x is a complete stack so there's no need for external modules. The database is MongoDB.
@@ -18,8 +16,8 @@ Testing environment
 Yes. It's a laptop.
 
 * Node.js v0.6.6
-* vert.x v1.0.1.final
-
+* vert.x v2.1.1
+* JDK 8u5 (Nashorn JavaScript Engine)
 * ulimit = unlimited
 
 Default V8/JVM and Node.js/vert.x settings unless stated otherwise.
--- vert.js
+++ vert.js
@@ -1,38 +1,29 @@
 // loading vertx shim
-
-load('vertx.js');
+var vertx = require('vertx.js');
+var container = require('vertx/container');
 
 // getting EventBus handler
-
 var eb = vertx.eventBus;
 
 // setting up MongoDB persistor
-
 var mongo = 'vertx.mongopersistor';
 
-vertx.deployVerticle('mongo-persistor', null, 1, function() {
-
+container.deployVerticle('mongo-persistor', null, 1, function() {
   // clearing database
   eb.send(mongo, {action: 'delete', collection: 'vertx', matcher: {}});
-
 });
 
 // setting up server
-
 var server = vertx.createHttpServer();
-
 var routeMatcher = new vertx.RouteMatcher();
 
 // post route handler
-
 routeMatcher.get('/post', function(req, res) {
   
   var response = '';
-
   var startDate = new Date();
 
   // database access
-
   eb.send(mongo, {action: 'save',
                   collection: 'vertx',
                   document: {
@@ -40,152 +31,100 @@ routeMatcher.get('/post', function(req, res) {
                     date: new Date().toString(),
                     content: randomString(160)
                   }},
-
   function(reply) {
-    
     if (reply.status === 'ok') {
-      
       eb.send(mongo, {action: 'find',
                       collection: 'vertx',
                       limit: 100,
                       matcher: {}},
-
       function(reply) {
-        
         if (reply.status === 'ok') {
-          
           response = JSON.stringify(reply.results);
-
           var endDate = new Date();
-
-          req.response.putAllHeaders({
-            'Content-Type': 'application/json',
-            'Date': endDate.toString(),
-            'Connection': 'close',
-            'X-Response-Time': endDate - startDate/*,
-            'Server': 'vert.x vs Node.js server'*/
-          });
-
+		  req.response.headers = {
+			'Content-Type': 'application/json',
+			'Date': endDate.toString(),
+			'Connection': 'close',
+			'X-Response-Time': endDate - startDate
+		  };
           req.response.end(response);
-          req.response.close();
-          
         } else {
-          
           request.response.statusCode = 500;
-
           var endDate = new Date();
-
-          req.response.putAllHeaders({
-            'Content-Type': 'application/json',
-            'Date': endDate.toString(),
-            'Connection': 'close',
-            'X-Response-Time': endDate - startDate/*,
-            'Server': 'vert.x vs Node.js server'*/
-          });
-
+		  req.response.headers = {
+			'Content-Type': 'application/json',
+			'Date': endDate.toString(),
+			'Connection': 'close',
+			'X-Response-Time': endDate - startDate
+		  };
           req.response.end(response);
-          req.response.close();
-
         }
-
       });
-
     } else {
-      
       request.response.statusCode = 500;
-
       var endDate = new Date();
-
-      req.response.putAllHeaders({
-        'Content-Type': 'application/json',
-        'Date': endDate.toString(),
-        'Connection': 'close',
-        'X-Response-Time': endDate - startDate/*,
-        'Server': 'vert.x vs Node.js server'*/
-      });
-
+	  req.response.headers = {
+		'Content-Type': 'application/json',
+		'Date': endDate.toString(),
+		'Connection': 'close',
+		'X-Response-Time': endDate - startDate
+	  };
       req.response.end(response);
-      req.response.close();
-
     }
-
   });
-
 });
 
 // hello route handler
-
 routeMatcher.get('/hello', function(req, res) {
-
   var startDate = new Date();
-
   var endDate = new Date();
 
-  req.response.putAllHeaders({
+  req.response.headers = {
     'Content-Type': 'application/json',
     'Date': endDate.toString(),
     'Connection': 'close',
-    'X-Response-Time': endDate - startDate/*,
-    'Server': 'vert.x vs Node.js server'*/
-  });
-
+    'X-Response-Time': endDate - startDate
+  };
+  
   req.response.end(JSON.stringify({message: 'hello'}));
-  req.response.close();
-
 });
 
 // concat route handler
-
 routeMatcher.get('/concat', function(req, res) {
-
   var startDate = new Date();
-
   var response = randomString(10000);
-
   var endDate = new Date();
 
-  req.response.putAllHeaders({
+  req.response.headers = {
     'Content-Type': 'application/json',
     'Date': endDate.toString(),
     'Connection': 'close',
-    'X-Response-Time': endDate - startDate/*,
-    'Server': 'vert.x vs Node.js server'*/
-  });
+    'X-Response-Time': endDate - startDate
+  };
 
   req.response.end(JSON.stringify({concat: response}));
-  req.response.close();
-
 });
 
 // fibonacci route handler
-
 routeMatcher.get('/fibonacci', function(req, res) {
-
   var startDate = new Date();
-
   fibonacci(30);
-
   var endDate = new Date();
 
-  req.response.putAllHeaders({
+  req.response.headers = {
     'Content-Type': 'application/json',
     'Date': endDate.toString(),
     'Connection': 'close',
-    'X-Response-Time': endDate - startDate/*,
-    'Server': 'vert.x vs Node.js server'*/
-  });
+    'X-Response-Time': endDate - startDate
+  };
 
   req.response.end(JSON.stringify({fibonacci: 'calculated'}));
-  req.response.close();
-
 });
 
-server.requestHandler(routeMatcher).listen(1338, 'localhost');
+server.requestHandler(routeMatcher).listen(1337, 'localhost');
 
 // helper function for random string generation
-
 var randomString = function(_len) {
-
   var alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
   var len = _len || 160;
@@ -198,11 +137,9 @@ var randomString = function(_len) {
   }
     
   return result;
-
 };
 
 // helper Fibonacci function
-
 var fibonacci = function(n) {
     if (n <= 1) return n;
     return fibonacci(n - 2) + fibonacci(n - 1);
